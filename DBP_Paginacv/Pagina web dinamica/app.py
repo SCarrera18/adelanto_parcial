cdd30bc37e9e49e7b461c1aaf9d7203661f6e1cc
SCarrera18 <131091849+SCarrera18@users.noreply.github.com>
2023-05-07 22:34:17 -0500

    Delete DBP_Paginacv/Pagina web dinamica directory

--- DBP_Paginacv/Pagina web dinamica/app.py
+++
@@ -1,4 +0,0 @@
-from my_app import app
-
-app.config.from_pyfile('config.py')
-app.run()
--- DBP_Paginacv/Pagina web dinamica/my_app/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-from my_app.fromulario.views import login
-
-app = Flask(__name__)
-app.register_blueprint(login)
